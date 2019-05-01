f523300bad1b86a9a6decfe50be2226f9e4493c5
Emmilie Estabillo <emsestabillo@gmail.com>
2019-05-01 16:20:40 -0400

    Add logic for the dice

--- app.js
+++ app.js
@@ -9,20 +9,39 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+////////
+//DICE
+///////
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  //1. random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2. display result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = "block";
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  //3. update round score but only if the rolled number is not a 1
+});
+
 
 //setter - set a value
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 //getter - reading the value
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none'
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
