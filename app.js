917401c2ac60a978dd219b518a248601491aa78a
Emmilie Estabillo <emsestabillo@gmail.com>
2019-05-01 17:24:20 -0400

    Implement hold and next player functions

--- app.js
+++ app.js
@@ -41,25 +41,51 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    //change current score to 0
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    //change the background and font weight by adding class
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
 
   }
 });
 
+////////
+//HOLD
+///////
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  //add current score to global score
+  scores[activePlayer] += roundScore;
+  
+  //update UI - update global score and active player
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //Check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+  } else {
+    nextPlayer();
+  }
+
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  //change current score to 0
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  //change the background, dot and font weight by adding class
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 //setter - set a value
 // document.querySelector('#current-' + activePlayer).textContent = dice;
